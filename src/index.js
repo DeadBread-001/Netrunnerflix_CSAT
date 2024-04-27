623636f9935e16d887820763fc785eafe1112489
DeadBread-001 <nikita10062004m@gmail.com>
2024-04-27 17:39:38 +0300

    обращение к беку

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import { renderMainPage } from "./components/main/main.js";
 import "../src/index.scss";
 
+let parentPageUrl;
 window.addEventListener("message", function (event) {
   if ("url" in event.data) {
-    const parentPageUrl = event.data.url;
-    console.log(parentPageUrl);
+    parentPageUrl = event.data.url;
+    if (parentPageUrl.includes("/profile")) {
+      renderMainPage("profileData");
+    } else if (parentPageUrl.includes("/actor/")) {
+      renderMainPage("actorData");
+    } else if (
+      parentPageUrl === "http://94.139.247.246:8080/" ||
+      parentPageUrl === "http://94.139.247.246:8080"
+    ) {
+      renderMainPage("filmsAll");
+    } else if (parentPageUrl.includes("/film/")) {
+      renderMainPage("filmData");
+    }
   }
 });
-
-// renderMainPage(parentPageUrl);
-renderMainPage("filmsAll");
